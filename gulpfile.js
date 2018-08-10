623b351041f8fa9bfcd1e022270438c22b6114f8
hopes91 <nadya.me91@gmail.com>
2018-08-10 10:56:59 +0300

    add gulp-remember, path, gulp-cached

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const del = require('del');
+const remember = require('gulp-remember');
+const path = require('path');
+const cached = require('gulp-cached');
 
 gulp.task('css', function() {
 	const plugins = [
@@ -16,10 +19,14 @@ gulp.task('css', function() {
 
 	return gulp.src('src/css/*.css')
 		.pipe(debug({title: 'src:'}))
-		.pipe(concatCss('style.css'))
-		.pipe(debug({title: 'concatCss:'}))
+		.pipe(cached('css'))
+		.pipe(debug({title: 'cached:'}))
 		.pipe(postcss(plugins))
 		.pipe(debug({title: 'autoprefixer:'}))
+		.pipe(remember('css'))
+		.pipe(debug({title: 'remember:'}))
+		.pipe(concatCss('style.css'))
+		.pipe(debug({title: 'concatCss:'}))
 		.pipe(uglifycss({"uglyComments": true}))
 		.pipe(debug({title: 'uglifycss:'}))
 		.pipe(gulp.dest('src/src'));
@@ -28,10 +35,14 @@ gulp.task('css', function() {
 gulp.task('js', function() {
 	return gulp.src('src/js/*.js')
 		.pipe(debug({title: 'src:'}))
-		.pipe(concat('script.js'))
-		.pipe(debug({title: 'concatjs:'}))
+		.pipe(cached('js'))
+		.pipe(debug({title: 'cached:'}))
 		.pipe(babel({presets: ['env']}))
 		.pipe(debug({title: 'babel:'}))
+		.pipe(remember('js'))
+		.pipe(debug({title: 'remember:'}))
+		.pipe(concat('script.js'))
+		.pipe(debug({title: 'concatjs:'}))
 		.pipe(uglify())
 		.pipe(debug({title: 'uglifyjs:'}))
 		.pipe(gulp.dest('src/src'));
@@ -47,8 +58,14 @@ gulp.task('build', gulp.series(
 );
 
 gulp.task('watch', function() {
-	gulp.watch('src/css/*.css', gulp.series('css'));
-	gulp.watch('src/js/*.js', gulp.series('js'));
+	gulp.watch('src/css/*.css', gulp.series('css')).on('unlink', function(filepath) {
+		remember.forget('css', path.resolve(filepath));
+		delete cached.caches.css[path.resolve(filepath)];
+	});
+	gulp.watch('src/js/*.js', gulp.series('js')).on('unlink', function(filepath) {
+		remember.forget('js', path.resolve(filepath));
+		delete cached.caches.js[path.resolve(filepath)];
+	});
 });
 
 gulp.task('default', gulp.series('build', 'watch'));
