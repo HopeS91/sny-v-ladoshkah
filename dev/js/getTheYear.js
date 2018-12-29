b079908502db715898fc247d34cdd160c19c7e99
hopes91 <nadya.me91@gmail.com>
2018-12-29 12:54:39 +0300

    change the copyright text and make the current year out of js

---
+++ dev/js/getTheYear.js
@@ -0,0 +1,10 @@
+const spanYear = document.getElementById('year');
+
+const getTheYear = () => {
+  let today = new Date();
+  let year = today.getFullYear();
+
+  spanYear.textContent = year;
+}
+
+window.addEventListener('load', getTheYear);
