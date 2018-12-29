655ce2433f5b187c3bb0a79d38678676b5e13a2d
hopes91 <nadya.me91@gmail.com>
2018-12-29 11:27:53 +0300

    add browserslist to package.json and change the syntax a little

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,76 @@
+'use strict';
+
 const gulp = require('gulp');
-const debug = require('gulp-debug');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const concatCss = require('gulp-concat-css');
 const uglifycss = require('gulp-uglifycss');
-const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const del = require('del');
-const remember = require('gulp-remember');
-const path = require('path');
 const cached = require('gulp-cached');
+const remember = require('gulp-remember');
+const del = require('del');
+const path = require('path');
 const browserSync = require('browser-sync').create();
 
 gulp.task('css', function() {
-	const plugins = [
-		autoprefixer({browsers: ['last 3 versions']})
-	];
-
-	return gulp.src('./dev/css/*.css')
-		.pipe(cached('css'))
-		.pipe(postcss(plugins))
-		.pipe(remember('css'))
+  return gulp.src('./dev/css/*.css')
+    .pipe(cached('css'))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(remember('css'))
 		.pipe(concatCss('style.css'))
-		.pipe(uglifycss({"uglyComments": true}))
-		.pipe(gulp.dest('./public'));
+    .pipe(uglifycss({"uglyComments": true}))
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('js', function() {
-	return gulp.src('./dev/js/*.js')
-		.pipe(cached('js'))
-		.pipe(babel({presets: ['env']}))
-		.pipe(remember('js'))
-		.pipe(concat('script.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./public'));
+  return gulp.src('./dev/js/*.js')
+    .pipe(cached('js'))
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(remember('js'))
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('assets', function() {
-	return gulp.src('./dev/assets/**/*.*', {since: gulp.lastRun('assets')})
-		.pipe(gulp.dest('./public'));
+  return gulp.src('./dev/assets/**/*.*', {since: gulp.lastRun('assets')})
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('clean', function() {
-	return del('./public');
+  return del('./public');
 });
 
 gulp.task('build', gulp.series(
-	'clean',
-	gulp.parallel('css', 'js', 'assets'))
+  'clean',
+  gulp.parallel('css', 'js', 'assets'))
 );
 
 gulp.task('watch', function() {
-	gulp.watch('./dev/css/*.css', gulp.series('css')).on('unlink', function(filepath) {
-		remember.forget('css', path.resolve(filepath));
-		delete cached.caches.css[path.resolve(filepath)];
-	});
+  gulp.watch('./dev/css/*.css', gulp.series('css')).on('unlink', function(filepath) {
+    remember.forget('css', path.resolve(filepath));
+    delete cached.caches.css[path.resolve(filepath)];
+  });
 
-	gulp.watch('./dev/js/*.js', gulp.series('js')).on('unlink', function(filepath) {
-		remember.forget('js', path.resolve(filepath));
-		delete cached.caches.js[path.resolve(filepath)];
-	});
+  gulp.watch('./dev/js/*.js', gulp.series('js')).on('unlink', function(filepath) {
+    remember.forget('js', path.resolve(filepath));
+    delete cached.caches.js[path.resolve(filepath)];
+  });
 
-	gulp.watch('./dev/assets/**/*.*', gulp.series('assets'));
+  gulp.watch('./dev/assets/**/*.*', gulp.series('assets'));
 });
 
 gulp.task('server', function() {
-	browserSync.init({
-		server: './public'
-	});
+  browserSync.init({
+    server: './public'
+  });
 
-	browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
+  browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.series(
-	'build',
-	gulp.parallel('watch', 'server'))
+  'build',
+  gulp.parallel('watch', 'server'))
 );
