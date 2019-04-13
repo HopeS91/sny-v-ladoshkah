ebcb3363c2333b2b7001f8ada818cca923a73d99
hopes91 <nadya.me91@gmail.com>
2019-04-13 21:20:50 +0300

    fix the script

--- dev/js/dropdownMenu.js
+++ dev/js/dropdownMenu.js
@@ -3,32 +3,39 @@ if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
-let dropdown = document.querySelectorAll('.dropdown');
+let burger = document.getElementById('burger');
 let burgerSpans = document.querySelectorAll('#burger span');
 let dropdownMenu = document.getElementById('drop-menu');
 
-const toggleBurger = event => {
-	burgerSpans[0].classList.toggle('span-one-active');
-	burgerSpans[2].classList.toggle('span-three-active');
-	setTimeout(() => {
-		burgerSpans[1].classList.toggle('span-two-active');
-	}, 200);
-
-	let parentId = event.target.parentNode.id;
-
-	if (event.target === burger || parentId === 'burger') {
-		showDropdown();
-	} else if (event.target === dropdownMenu) {
-		hideDropdown();
+const toggleTubindex = () => {
+	if (window.innerWidth <= 800) {
+		burger.setAttribute('tabindex', '0');
+	} else {
+		burger.setAttribute('tabindex', '-1');
 	}
 }
 
-const showDropdown = () => {
-	dropdownMenu.style.display = 'block';
+const toggleBurger = event => {
+	let parentId = event.target.parentNode.id;
+
+	if (event.target.id === 'burger' || parentId === 'burger') {
+		burgerSpans[0].classList.toggle('span-one-active');
+		burgerSpans[2].classList.toggle('span-three-active');
+		setTimeout(() => {
+			burgerSpans[1].classList.toggle('span-two-active');
+		}, 200);
+
+		toggleDropdownMenu();
+	}
 }
 
-const hideDropdown = () => {
-	dropdownMenu.style.display = 'none';
+const toggleDropdownMenu = () => {
+	if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
+		dropdownMenu.style.display = 'block';
+	} else {
+		dropdownMenu.style.display = 'none';
+	}
 }
 
-dropdown.forEach(element => element.addEventListener('click', toggleBurger));
+burger.addEventListener('click', toggleBurger);
+window.addEventListener('resize', toggleTubindex);
