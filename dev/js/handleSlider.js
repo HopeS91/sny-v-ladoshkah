120fc8a9da0a07863e7b230b0cc119d390d40b16
hopes91 <nadya.me91@gmail.com>
2019-04-18 13:35:31 +0300

    refactor

--- dev/js/handleSlider.js
+++ dev/js/handleSlider.js
@@ -18,45 +18,39 @@ const showFirstSlide = event => {
 	back.style.display = 'block';
 
 	let clickedAlt = event.target.getAttribute('alt');
-	let comparedAlt = null;
+	let altToCompare = null;
 
 	slides.forEach((slide, index) => {
-		comparedAlt = slide.getAttribute('alt');
+		altToCompare = slide.getAttribute('alt');
 		slide.style.display = 'none';
 
-		if (clickedAlt === comparedAlt) {
+		if (clickedAlt === altToCompare) {
 			slide.style.display = 'block';
 			mainSlide = index;
 			start = index;
-			currentSlide();
-		}
-
-		if (start === 0) {
-			prevSlide.style.display = 'none';
-		} else if (start === photoIcons.length - 1) {
-			nextSlide.style.display = 'none';
-		} else {
-			prevSlide.style.display = 'block';
-			nextSlide.style.display = 'block';
+			showSlides();
+			handleArrows();
 		}
 	});
 }
 
-const moveSlidesOnKeyDown = event => {
+const handleArrowsOnKeyDown = event => {
 	if (event.key === 'ArrowLeft') {
-		moveSlides(-1);
+		handleArrows(-1);
 	} else if (event.key === 'ArrowRight') {
-		moveSlides(1);
+		handleArrows(1);
 	}
 }
 
-const moveSlides = index => {
+const handleArrows = index => {
 	if ((index === -1 && mainSlide === 0) ||
 			(index === 1 && mainSlide === slides.length - 1)) {
 		index = null;
+	} else if (index === undefined) {
+		mainSlide = start;
+	} else {
+		mainSlide += index;
 	}
-
-	mainSlide += index;
 
 	if (mainSlide === 0) {
 		prevSlide.style.display = 'none';
@@ -70,6 +64,10 @@ const moveSlides = index => {
 		nextSlide.style.display = 'block';
 	}
 
+	showSlides();
+}
+
+const showSlides = () => {
 	slides.forEach(slide => slide.style.display = 'none');
 
 	for (let i = start; i < slides.length; i++) {
@@ -81,29 +79,29 @@ const moveSlides = index => {
 const currentSlide = () => {
 	let dots = document.querySelectorAll('.dot');
 
-	dots.forEach(dot => dot.style.backgroundColor = '#ccc');
+	dots.forEach(dot => dot.style.backgroundColor = 'oldlace');
 
 	for (let i = start; i < dots.length; i++) {
-		dots[mainSlide].style.backgroundColor = '#c471a3';
+		dots[mainSlide].style.backgroundColor = 'saddlebrown';
 	}
 }
 
-const hideSlidesOnKeyDown = event => {
+const hideSliderOnKeyDown = event => {
 	if (event.key === 'Escape') {
-		hideSlides(event);
+		hideSlider();
 	}
 }
 
-const hideSlides = event => {
+const hideSlider = () => {
 	mainSlide = null;
 	start = null;
 	back.style.display = 'none';
 }
 
 if (back) {
-	photoIcons.forEach(photoIcon => photoIcon.addEventListener('keydown', showFirstSlideOnKeyDown));
-	photoIcons.forEach(photoIcon => photoIcon.addEventListener('click', showFirstSlide));
-	window.addEventListener('keydown', moveSlidesOnKeyDown);
-	window.addEventListener('keydown', hideSlidesOnKeyDown);
-	closeIcon.addEventListener('click', hideSlides);
+	photoIcons.forEach(photo => photo.addEventListener('keydown', showFirstSlideOnKeyDown));
+	photoIcons.forEach(photo => photo.addEventListener('click', showFirstSlide));
+	window.addEventListener('keydown', handleArrowsOnKeyDown);
+	window.addEventListener('keydown', hideSliderOnKeyDown);
+	closeIcon.addEventListener('click', hideSlider);
 }
